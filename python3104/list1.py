45cece82377dc888ed6363642613ed8dda86ce18
kommusravani <104352484+kommusravani@users.noreply.github.com>
2022-05-26 12:00:07 +0530

    Add files via upload

---
+++ python3104/list1.py
@@ -0,0 +1,10 @@
+# a=[4,"varun",6.6,[11,3.4]]
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# # print(a[4])
+# print(a[3][0])
+# print(a[3][1],type(a[2])) #3.4 float
+
+print(dir(list))
